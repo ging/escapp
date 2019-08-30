db803168e14a710eaeb8316f4e294c17c2241486
Sonsoles López Pernas <sonsoleslp@hotmail.com>
2019-08-30 19:39:19 +0200

    Remove migration

--- migrations/20190502162450-AddAutomaticFieldToEscapeRoomsTable.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-module.exports = {
-
-    "up": (queryInterface, Sequelize) => queryInterface.addColumn("escapeRooms", "automatic", Sequelize.BOOLEAN, {"defaultValue": false}),
-    "down": (queryInterface, Sequelize) => queryInterface.removeColumn("escapeRooms", "automatic", Sequelize.BOOLEAN)
-
-};
