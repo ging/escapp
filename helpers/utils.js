f6a9de0c8c016680086b4be47f9c6080099a5c86
sonsoleslp <sonsoleslp@hotmail.com>
2019-08-30 18:29:09 +0200

    Minor refactor

--- helpers/utils.js
+++ helpers/utils.js
@@ -11,3 +11,44 @@ exports.retosSuperadosByWho = (who, puzzles) => {
     });
     return retosSuperados;
 };
+
+exports.flattenObjet = (obj, labels) => {
+    let rs = {};
+
+    for (const r in obj) {
+        rs[labels[r]] = obj[r];
+    }
+    return rs;
+};
+
+exports.getRetosSuperados = (teams) => {
+    return teams.map((teamRes) => ({...teamRes.dataValues,
+        "countretos": teamRes.dataValues.retos.length,
+        "latestretosuperado": teamRes.dataValues.retos && teamRes.dataValues.retos.length > 0 ? teamRes.dataValues.retos.map((r) => r.retosSuperados.createdAt).sort((a, b) => a < b)[0] : null})).sort((t1, t2) => {
+        if (t1.countretos === t2.countretos) {
+            if (t1.latestretosuperado === t2.latestretosuperado) {
+                return 0;
+            }
+            return t1.latestretosuperado > t2.latestretosuperado ? 1 : -1;
+        }
+        return t1.countretos > t2.countretos ? -1 : 1;
+    });
+}
+
+exports.pctgRetosSuperados = (retosSuperados) => {
+    return Math.round(retosSuperados.filter((r) => r === 1).length * 10000 / retosSuperados.length) / 100;
+}
+
+exports.countHints = (requestedHints) => {
+    let hintsSucceeded = 0;
+    let hintsFailed = 0;
+    for (const h in requestedHints) {
+        const hint = requestedHints[h];
+
+        if (hint.success) {
+            hintsSucceeded++;
+        } else {
+            hintsFailed++;
+        }
+    }
+}
