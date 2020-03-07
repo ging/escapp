e8f388263d56478529be22c462b8bdca98530bcf
sonsoleslp <sonsoleslp@hotmail.com>
2020-03-07 12:58:07 +0100

    Add heroku env

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const index = require("./routes/index"),
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-if (app.get("env") === "production") {
+if (app.get("env") === "production" && !process.env.HEROKU) {
     app.use((req, res, next) => {
         if (req.secure) {
             next();
